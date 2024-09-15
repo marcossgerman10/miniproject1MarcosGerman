621d1ab8fc67cc506a4e6b515b56af924c6d1f99
marcos <m_german4@mail.fhsu.edu>
2024-09-14 21:37:25 -0500

    Add plotting and saving of graphs as PNG files

--- stock_graphs.py
+++ stock_graphs.py
@@ -7,7 +7,7 @@ import numpy as np
 import yfinance as yf
 import os
 
-stock_tickers = ['NFLX', 'MSFT', 'APPL', 'GOOGL', 'DIS']
+stock_tickers = ['NFLX', 'MSFT', 'AAPL', 'GOOGL', 'DIS']
 data = {}
 
 for stock_ticker in stock_tickers:
@@ -16,3 +16,14 @@ for stock_ticker in stock_tickers:
     data[stock_ticker] = history['Close'].tolist()
 
 stock_arrays = {stock_ticker: np.array(prices) for stock_ticker, prices in data.items()}
+
+if not os.path.exists('charts'):
+    os.makedirs('charts')
+
+for ticker, prices in stock_arrays.items():
+    plt.plot(prices, marker='o')
+    plt.title(f'{ticker} Closing Prices (Last 10 Days)')
+    plt.xlabel('Days')
+    plt.ylabel('Closing Price (USD)')
+    plt.savefig(f'charts/{ticker}_closing_prices.png')
+    plt.clf()
