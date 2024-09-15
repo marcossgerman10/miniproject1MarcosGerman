554205c93f283f5eb42a7b85de2181c1327af413
marcos <m_german4@mail.fhsu.edu>
2024-09-14 21:40:07 -0500

    change to valid period

--- stock_graphs.py
+++ stock_graphs.py
@@ -12,8 +12,8 @@ data = {}
 
 for stock_ticker in stock_tickers:
     stock = yf.Ticker(stock_ticker)
-    history = stock.history(period='10d')
-    data[stock_ticker] = history['Close'].tolist()
+    history = stock.history(period='1mo')
+    data[stock_ticker] = history['Close'].tail(10).tolist()
 
 stock_arrays = {stock_ticker: np.array(prices) for stock_ticker, prices in data.items()}
 
