2c28136fe796b245f7245ef28708a0dc08c0bcb6
marcos <m_german4@mail.fhsu.edu>
2024-09-14 20:12:41 -0500

    Fetch closing prices for stock tickers using yfinance

--- stock_graphs.py
+++ stock_graphs.py
@@ -6,3 +6,11 @@ import matplotlib.pyplot as plt
 import numpy as np
 import yfinance as yf
 import os
+
+stock_tickers = ['NFLX', 'MSFT', 'APPL', 'GOOGL', 'DIS']
+data = {}
+
+for stock_ticker in stock_tickers:
+    stock = yf.Ticker(stock_ticker)
+    history = stock.history(period='10d')
+    data[stock_ticker] = history['Close'].tolist()
